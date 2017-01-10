b5753617d01b3fac00079763da0472aefcaba9b3
Joshua Ellis <jellis@jellis.com.au>
2017-01-11 08:08:12 +1100

    Pushing to a minor version for simpler updates on backwards compatibility

--- src/Scope.php
+++ src/Scope.php
@@ -7,7 +7,7 @@ use Jellis\Check\Facades\Check;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Scope as EloquentScope;
+use Illuminate\Database\Eloquent\ScopeInterface as EloquentScope;
 
 class Scope implements EloquentScope
 {
@@ -30,6 +30,10 @@ class Scope implements EloquentScope
         }
 
         return $builder;
+    }
+
+    public function remove(Builder $builder, Model $model) {
+        // Only implemented to guarantee interface compatibility
     }
 
 }
