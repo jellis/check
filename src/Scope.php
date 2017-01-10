6f69c65d6121950b43a4a3839d28ddc979795c2b
Joshua Ellis <jellis@jellis.com.au>
2017-01-11 07:51:20 +1100

    Bumped required version due to deprecated ScopeInterface interface

--- src/Scope.php
+++ src/Scope.php
@@ -7,7 +7,7 @@ use Jellis\Check\Facades\Check;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\ScopeInterface as EloquentScope;
+use Illuminate\Database\Eloquent\Scope as EloquentScope;
 
 class Scope implements EloquentScope
 {
